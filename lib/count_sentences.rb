f71784067ca29fde4eb80afa1c082b99407328ad
kelvin901 <wawerukelvin2019@gmail.com>
2023-05-23 11:36:19 +0300

    Completed Assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,16 @@
 require 'pry'
 
 class String
-
   def sentence?
-    
-  end
-
-  def question?
-
-  end
-
-  def exclamation?
-
-  end
-
-  def count_sentences
-
+    self[-1] == '.'
   end
 end
+
+puts "Hello.".sentence?  #output true
+puts "Goodbye".sentence? # output false
+puts "Yes!".sentence?    # output false
+
+#This code defines an instance method sentence? on the String class. Within the method, self refers to the instance of the string on which the method is called.
+
+#The sentence? method checks if the last character of the string (self[-1]) is equal to a period ('.'). If it is, the method returns true; otherwise, it returns false.
+
